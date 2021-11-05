6e0931dc855a3ec439d101ed976e67ca1b2659e7
alekoChe <aleko2117bis@gmail.com>
2021-11-05 11:52:44 +0300

    add constructor

--- src/main/java/AppHelloWorld.java
+++ src/main/java/AppHelloWorld.java
@@ -1,6 +1,10 @@
 public class AppHelloWorld {
     private String hello = "Hello world!";
 
+    public AppHelloWorld() {
+        this.hello = hello;
+    }
+
     public static void main(String[] args) {
         int n = 5;
     }
